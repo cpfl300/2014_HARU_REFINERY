038465d9ba2434ab43559ea17f7eed1e74b1998b
cocagolau <cocagolau@gmail.com>
2014-11-04 11:14:14 +0900

    file에 log를 저장할 수 있도록 설정
    $HOME/log/refinery/refinery.log

--- src/main/java/refinery/main/Main.java
+++ src/main/java/refinery/main/Main.java
@@ -15,6 +15,8 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 		log.debug("context: " + context);
+		log.debug("context: " + context);
+		log.debug("context: " + context);
 		
 		Engine engine = context.getBean("engine", Engine.class);
 		engine.start();
