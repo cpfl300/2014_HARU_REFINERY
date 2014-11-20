86c21e945d98b7bbe9254ee109e5625025445d1d
cocagolau <cocagolau@gmail.com>
2014-11-20 16:59:04 +0900

    issue #162
    https://github.com/cpfl300/2014_HARU/issues/162
    
    quartz 적용중

--- src/main/java/refinery/config/Config.java
+++ src/main/java/refinery/config/Config.java
@@ -4,14 +4,23 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.quartz.CronTrigger;
+import org.quartz.JobDataMap;
+import org.quartz.Trigger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import refinery.job.detail.NaverNewsJobBean;
+import refinery.job.task.NaverNewsTask;
 
 @org.springframework.context.annotation.Configuration
 @ComponentScan(basePackages={"refinery", "core"})
@@ -50,4 +59,48 @@ public class Config {
 		
 		return env.getRequiredProperty("naver.news.api");
 	}
+	
+	@Bean
+	public JobDetailFactoryBean naverNewsJobBean() {
+		JobDetailFactoryBean jobBean = new JobDetailFactoryBean();
+		jobBean.setJobClass(NaverNewsJobBean.class);
+		jobBean.setDurability(true);
+		
+		JobDataMap jobDataMap = new JobDataMap();
+		jobDataMap.put("NaverNewsTask", NaverNewsTask.class);
+		
+		jobBean.setJobDataMap(jobDataMap);
+		
+		return jobBean;
+	}
+	
+	
+	@Bean
+	public CronTrigger cronTrigger() {
+		
+		CronTriggerFactoryBean cronTriggerFactory = new CronTriggerFactoryBean();
+		cronTriggerFactory.setStartDelay(1000);
+		cronTriggerFactory.setCronExpression("0/3 * * * * ?");
+		
+		return cronTriggerFactory.getObject();
+		
+	}
+	
+	@Bean
+	public SchedulerFactoryBean schedulerFactoryBean() {
+		SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
+		
+		Trigger[] triggerArr = new Trigger[1];
+		triggerArr[0] = cronTrigger();
+		
+		scheduler.setTriggers(triggerArr);
+		
+		return scheduler;
+		
+	}
+	
+	
+	
+	
+	
 }
