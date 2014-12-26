ddc2db7b30d431fa64a8199aee4ade927a64f3ee
cocagolau <cocagolau@gmail.com>
2014-12-27 02:59:57 +0900

    issue #258
    https://github.com/cpfl300/2014_HARU/issues/258
    
    핫이슈 저장을 시간당 1회 호출하지 않고
    알고리즘을 마무리한 시점에 부를 수 있독 변경 구현
    
    테이블 설계

--- src/main/java/refinery/scheduler/TaskScheduler.java
+++ src/main/java/refinery/scheduler/TaskScheduler.java
@@ -1,16 +1,18 @@
-package refinery.service;
+package refinery.scheduler;
 
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import elixir.status.StatusArchive;
+import refinery.service.RefineryService;
+import elixir.archive.StatusArchive;
 import elixir.utility.ElixirUtils;
 
-@Service
-public class ScheduleService {
+@Component
+public class TaskScheduler {
 	
 	@Autowired
 	private RefineryService refineryService;
@@ -24,15 +26,26 @@ public class ScheduleService {
 		statusArchive.sync(now);
 	}
 	
-	@Scheduled(cron="* 0/10 * * * ?")
-	public void saveHotissueList() {
-		refineryService.saveHotissueList();
+
+	@Scheduled(cron="0 50 5/12 * * ?")
+	public void execAlgorithm() {
+		
+		// execAlgorithm
+		
+		// 
+		saveHotissueList();
 	}
+	
 	
 	@Scheduled(cron="* 0/10 * * * ?")
 	public void saveArticleList() {
 		String datehour = ElixirUtils.format("yyyyMMddHHmm", ElixirUtils.now()).substring(0, 11);
 		refineryService.saveArticleList(datehour);
+	}
+	
+	@Async
+	private void saveHotissueList() {
+		refineryService.saveHotissueList();
 	}
 	
 }
