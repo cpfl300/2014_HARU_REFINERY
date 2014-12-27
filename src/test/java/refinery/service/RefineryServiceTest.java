0a2e5546bd7a306be9d3b9c9ea87f0d2360f3c45
cocagolau <cocagolau@gmail.com>
2014-12-27 11:30:19 +0900

    issue #259
    https://github.com/cpfl300/2014_HARU/issues/259
    
    RefineryService에서 saveHotissueList구현

--- src/test/java/refinery/service/RefineryServiceTest.java
+++ src/test/java/refinery/service/RefineryServiceTest.java
@@ -1,12 +1,11 @@
 package refinery.service;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,25 +14,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import refinery.model.NaverArticle;
-import refinery.model.NaverArticleTest;
-import refinery.model.NaverHotissue;
-import refinery.model.NaverHotissueTest;
-import refinery.model.NaverHotissues;
-import refinery.model.NaverSection;
-import refinery.model.NaverSectionTest;
-import refinery.model.NaverSections;
-import refinery.model.NaverSectionsTest;
-import elixir.model.Article;
-import elixir.model.ArticlesTest;
+import elixir.archive.StatusArchive;
 import elixir.model.Hotissue;
-import elixir.model.OfficesTest;
+import elixir.model.HotissueTest;
 import elixir.model.Section;
-import elixir.model.Signature;
-import elixir.model.SignaturesTest;
-import elixir.service.ArticleService;
+import elixir.model.Status;
 import elixir.service.HotissueService;
-import elixir.utility.ElixirUtils;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RefineryServiceTest {
@@ -42,119 +28,45 @@ public class RefineryServiceTest {
 	private RefineryService refineryService;
 	
 	@Mock
-	private NaverService naverServiceMock;
+	private NaverService naverService;
 	
 	@Mock
-	private HotissueService hotissueServiceMock;
+	private HotissueService hotissueService;
 	
 	@Mock
-	private ArticleService articleServiceMock;
-
+	private StatusArchive statusArcive;
 	
-	private List<NaverHotissue> naverHotissues;
-	private List<NaverArticle> naverArticles;
-	private List<NaverSection> naverSections;
-	
-	private String datehour;
-
-	private List<Article> articles;
 	private List<Hotissue> hotissues;
-	private List<Section> sections;
-	private List<Signature> signatures;
+	private Status status;
+
 	
 	@Before
 	public void setup() {
-		datehour = ElixirUtils.format("yyyyMMddHHmm", ElixirUtils.now()).substring(0, 11);
-		
-		naverSections = NaverSectionTest.preparedList();	
-		sections = NaverSections.convert(naverSections);
-		
-		naverHotissues = NaverHotissueTest.preparedList();
-		hotissues = NaverHotissues.convert(naverHotissues);
-		
-		signatures = SignaturesTest.preparedList();
-		
-		prepareArticles();
+		status = new Status("20141207", false);
 	}
-	
 
+	
 	@Test
-	public void saveHotissueList() {		
-		// mock
-		when(naverServiceMock.getHotissueList()).thenReturn(hotissues);
+	public void saveHotissueList() {
+		// given
+		prepareHotissues();
+		when(statusArcive.getStatus()).thenReturn(status);
+		when(naverService.getHotissueList()).thenReturn(hotissues);
 		
-		// exec
+		// when
 		refineryService.saveHotissueList();
 		
-		// vierfy
-		verify(hotissueServiceMock, times(1)).addAll(hotissues);
-	}
-	
-	
-//	@Test
-//	public void saveArticleList() {
-//		// mock
-//		when(naverServiceMock.getArticleList(datehour)).thenReturn(articles);
-//		
-//		// exec
-//		refineryService.saveArticleList();
-//		
-//		// verify
-//		verify(articleServiceMock, times(1)).addAll(any());
-//		
-//	}
-//	
-//	
-//	@Test
-//	public void async_updateContentOfArticleList() throws InterruptedException, ExecutionException {
-//		
-//		
-//		assertThat(refineryService.updateContentOfArticleList(signatures).get(), is(-2));
-//	}
-//	
-//	@Test
-//	public void async_updateContentOfArticle() throws InterruptedException, ExecutionException {
-//		
-//		assertThat(refineryService.updateContentOfArticle(signatures.get(0)).get(), is(-2));
-//	}
-//	
-//	@Test
-//	public void updateConentOfArticle() {
-//		
-//		for (int i=0; i<signatures.size(); i++) {
-//			Signature signature = signatures.get(i);
-//			
-//			when(naverServiceMock.getArticle(signature)).thenReturn(articles.get(0));
-//						
-//			refineryService.updateContentOfArticle(signature);
-//			
-//		}
-//		verify(articleServiceMock, times(3)).updateContent(any());
-//		
-//	}
-
-	
-	
-	private void prepareArticles() {
-		List<List<NaverSection>> naverSectionsList = new ArrayList<List<NaverSection>>();
-		List<NaverSection> ns1 = NaverSectionsTest.preparedList1();
-		List<NaverSection> ns2 = NaverSectionsTest.preparedList2();
-		List<NaverSection> ns3 = NaverSectionsTest.preparedList3();
-		naverSectionsList.add(ns1);
-		naverSectionsList.add(ns2);
-		naverSectionsList.add(ns3);
-		
-		List<Section> s1 = NaverSections.convert(ns1);
-		List<Section> s2 = NaverSections.convert(ns2);
-		List<Section> s3 = NaverSections.convert(ns3);
-		List<List<Section>> ss = new ArrayList<List<Section>>();
-		ss.add(s1);
-		ss.add(s2);
-		ss.add(s3);
-		
-		naverArticles = NaverArticleTest.preparedList(naverSectionsList);
-		articles = ArticlesTest.preparedList(OfficesTest.preparedList(), ss);
+		// then
+		verify(hotissueService, times(1)).addAll(status, hotissues);
 	}
 	
 
+	private void prepareHotissues() {
+		hotissues = Arrays.asList(new Hotissue[]{
+			HotissueTest.create("887522", "연애지침서", new Section("103")),	
+			HotissueTest.create("893847", "화제의 판결", new Section("102")),	
+			HotissueTest.create("895439", "지구촌 화제", new Section("104"))	
+		});
+		
+	}
 }
