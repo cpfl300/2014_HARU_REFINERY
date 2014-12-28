15545d9cfabbcaa3ad53a09b9a2bc893ecbde92c
cocagolau <cocagolau@gmail.com>
2014-12-29 07:02:14 +0900

    fix

--- src/main/java/refinery/model/NaverHotissue.java
+++ src/main/java/refinery/model/NaverHotissue.java
@@ -1,9 +1,11 @@
 package refinery.model;
 
 import elixir.model.Hotissue;
+import elixir.model.Section;
 
 
 public class NaverHotissue implements Convertible<Hotissue>{
+	private static final String SECTION_ID_EXP = "sid1=";
 	
 	private String panelId;
 	private String componentId;
@@ -47,8 +49,17 @@ public class NaverHotissue implements Convertible<Hotissue>{
 		
 		hotissue.setHotissueId(this.componentId);
 		hotissue.setTitle(this.title);
+		hotissue.setSection(new Section(extractSectionId()));
 		
 		return hotissue;
 	}
+
+	private String extractSectionId() {
+		String[] strs = this.url.split(NaverHotissue.SECTION_ID_EXP);
+		String sectionId = strs[1].substring(0,3); 
+
+		return sectionId;
+	}
+	
 
 }
